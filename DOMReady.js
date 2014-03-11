b6a19a127d3d9dc2af5ab1006ba6ee5ca4871268
mescoda <mescodasun@gmail.com>
2014-03-11 19:25:13 +0800

    init done

---
+++ DOMReady.js
@@ -0,0 +1,40 @@
+(function() {
+    function domready(fn) {
+        var isInsideFrame = (top != self),
+            done = false;
+
+        function readyHandler() {
+            if(done) {
+                return;
+            }
+            done = true;
+            fn();
+        }
+
+        if(document.readyState === 'complete') {
+            readyHandler();
+        } else {
+            if(document.addEventListener) {
+                document.addEventListener('DOMContentLoaded', readyHandler, false);
+            } else if(document.attachEvent) {
+                if(document.documentElement.doScroll && !isInsideFrame) {
+                    function doScrollCheck() {
+                        try {
+                            document.documentElement.doScroll('left');
+                            readyHandler();
+                        } catch(e) {
+                            setTimeout(doScrollCheck, 10);
+                        }
+                    }
+                    doScrollCheck();
+                }
+                document.attachEvent('onreadystatechange', function() {
+                    if(document.readyState === 'complete') {
+                        readyHandler();
+                    }
+                });
+            }
+        }
+    }
+    window.domready = domready;
+})();
